a8c2d4ca9a316b94a1619cd483db7ec0ad95eaaa
Rohan Gope <rg2512@it.jgec.ac.in>
2024-06-22 20:40:32 +0530

    Refactor TopPlay component to remove unused code and dependencies

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -4,11 +4,8 @@ import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { FreeMode } from 'swiper';
-
 import PlayPause from './PlayPause';
 import { playPause, setActiveSong } from '../redux/features/playerSlice';
-// import { useGetTopChartsQuery } from '../redux/services/shazamCore';
-
 import 'swiper/css';
 import 'swiper/css/free-mode';
 import { topPlayData } from '../assets/constants';
